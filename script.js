fa9005d09632e290ffa5e434fe546962cd56f10d
Carlos yuri <yurisousaenfer@gmail.com>
2023-01-31 09:32:44 -0300

    adjust end commit

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ function toggleMode(){
     const html = document.querySelector("html")
     html.classList.toggle("light")
     
+    const profile = document.querySelector("#profile img")
+    if (html.classList.contains("light")){
+        profile.setAttribute("src","./assets/profile-light.svg")
+    }else{
+        profile.setAttribute("src","./assets/profile-yondaime.svg")
+
+    }
 }
